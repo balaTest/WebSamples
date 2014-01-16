93bbae080363cb6aa52080f5b37ea3bc7b6056b8
balaTest <balaTest@live.com>
2014-01-15 17:52:15 -0800

    Add Admin action and view pages (without the authorization code)

--- WebApiBasicSamples/ProductStore2/ProductStore2/Controllers/HomeController.cs
+++ WebApiBasicSamples/ProductStore2/ProductStore2/Controllers/HomeController.cs
@@ -26,5 +26,13 @@ namespace ProductStore2.Controllers
 
             return View();
         }
+
+        public ActionResult Admin()
+        {
+            string apiUri = Url.HttpRouteUrl("DefaultApi", new { controller = "admin", });
+            ViewBag.ApiUrl = new Uri(Request.Url, apiUri).AbsoluteUri.ToString();
+
+            return View();
+        }
     }
 }
