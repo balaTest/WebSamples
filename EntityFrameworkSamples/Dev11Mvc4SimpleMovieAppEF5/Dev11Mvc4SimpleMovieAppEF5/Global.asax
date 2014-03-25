a038a62a631beee34a412bb4a9af28013c93cb62
Bala Chirtsabesan <balaTest@live.com>
2014-03-25 07:52:41 -0700

    Created solution Dev11Mvc4SimpleMovieAppEF5

---
+++ EntityFrameworkSamples/Dev11Mvc4SimpleMovieAppEF5/Dev11Mvc4SimpleMovieAppEF5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dev11Mvc4SimpleMovieAppEF5.MvcApplication" Language="C#" %>
---
+++ EntityFrameworkSamples/Dev11Mvc4SimpleMovieAppEF5/Dev11Mvc4SimpleMovieAppEF5/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Dev11Mvc4SimpleMovieAppEF5.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieContext : DbContext
+    {
+        public MovieContext()
+            : base("name=Movies")
+        // C# will call base class parameterless constructor by default
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
